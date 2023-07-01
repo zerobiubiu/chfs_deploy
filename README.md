08e647c5ddd600f23452a86c55ad5761252333b5
zerobiubiu <1731865922@qq.com>
2023-07-01 09:30:57 +0800

    end

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 > 一键部署 Linux 下的文件服务器
 
+本程序依赖于 [chfs][http://iscute.cn/chfs] 提供 Linux 下的一键部署脚本，更多详情请参考 chfs 官网
+
+## 安装脚本
+
+运行安装脚本您可以手动下载并运行脚本，或者使用以下 curl 或 wget 命令：
+
+**CURL：**
+
+```sh
+curl -o- https://cdn.jsdelivr.net/gh/zerobiubiu/chfs_deploy@main/install.sh | sh
+```
+
+**WGET：**
+
+```sh
+wget -qO- https://cdn.jsdelivr.net/gh/zerobiubiu/chfs_deploy@main/install.sh | sh
+```
--- install.sh
+++ install.sh
@@ -54,7 +54,117 @@ if which git >/dev/null; then
     done
 
     sudo cp -rf /tmp/chfs_deploy/chfs /opt/
+    if [ -f "/opt/chfs/log/.tmp" ]; then
+        sudo rm -rf /opt/chfs/log/.tmp
+    fi
+    if [ -f "/opt/chfs/resource/.tmp" ]; then
+        sudo rm -rf /opt/chfs/resource/.tmp
+    fi
 
+    text=$(
+        cat <<EOF
+[Unit]
+Description=chfs
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/chfs/start.sh
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+    )
+    output_dir="/etc/systemd/system/"
+    output_file="$output_dir/chfs.service"
+    mkdir -p "$output_dir"
+
+    sudo echo "$text" >"$output_file"
+
+    read -p "设置服务器启动端口（0-65535）: （44380）" input
+
+    # 判断输入是否匹配正则表达式
+    if [[ $input =~ ^[0-9]+$ && $input -ge 0 && $input -le 65535 ]]; then
+        echo "# 端口设置" >>/opt/chfs/config/conf.ini
+        echo "port=$input" >>/opt/chfs/config/conf.ini
+        echo "已设置服务器端口为 $input"
+    elif [[ -z $input ]]; then
+        echo "已设置服务器端口为 44380"
+        echo "# 端口设置" >>/opt/chfs/config/conf.ini
+        echo "port=44380" >>/opt/chfs/config/conf.ini
+    else
+        echo "输入无效，不是一个 0-65535 的数字。"
+        echo "已设置服务器端口为 44380"
+        echo "# 端口设置" >>/opt/chfs/config/conf.ini
+        echo "port=44380" >>/opt/chfs/config/conf.ini
+    fi
+
+    read -p "请设置匿名用户权限（R=读，W=写，D=删（输入对应字母及对应权限，如 RWD））: （无权）" input
+    input=$(echo "$input" | tr '[:lower:]' '[:upper:]')
+    if [[ $input =~ ^[RWD]{1,3}$ ]]; then
+        echo "# 匿名用户设置" >>/opt/chfs/config/conf.ini
+        echo "rule=::$input" >>/opt/chfs/config/conf.ini
+        echo "已设置匿名用户权限为 $input"
+    elif [[ -z $input ]]; then
+        echo "# 匿名用户设置" >>/opt/chfs/config/conf.ini
+        echo "rule=::" >>/opt/chfs/config/conf.ini
+        echo "已设置匿名用户无权限"
+    else
+        echo "输入无效，不是一个合法权限。"
+        echo "# 匿名用户设置" >>/opt/chfs/config/conf.ini
+        echo "rule=::" >>/opt/chfs/config/conf.ini
+        echo "已设置匿名用户无权限"
+    fi
+
+    read -p "请输入用户名（留空将使用默认用户名 user）：" input
+    if [[ -z $input ]]; then
+        username="user"
+    else
+        username=$input
+    fi
+
+    read -p "请输入用户密码（留空将使用默认密码 passwd）：" input
+    if [[ -z $input ]]; then
+        password="passwd）："
+    else
+        password=$input
+    fi
+
+    read -p "请设置用户权限（R=读，W=写，D=删（输入对应字母及对应权限，如 RWD））: （RWD）" input
+    input=$(echo "$input" | tr '[:lower:]' '[:upper:]')
+    if [[ $input =~ ^[RWD]{1,3}$ ]]; then
+        power=$input
+    elif [[ -z $input ]]; then
+        power="RWD"
+    else
+        echo "输入无效，不是一个合法权限。"
+        echo "已设置用户权限为 RWD"
+    fi
+
+    echo "# 用户设置" >>/opt/chfs/config/conf.ini
+    echo "rule=$username:$password:$power" >>/opt/chfs/config/conf.ini
+
+    echo "# 服务器标题设置" >>/opt/chfs/config/conf.ini
+    echo "html.title="$username 的文件服务器"" >>/opt/chfs/config/conf.ini
+
+    sudo systemctl daemon-reload
+    if [ $? -eq 0 ]; then
+        echo "服务重载成功。。。"
+    fi
+    sudo systemctl start chfs.service
+    if [ $? -eq 0 ]; then
+        echo "服务启动成功。。。"
+    fi
+    sudo systemctl enable chfs.service
+    if [ $? -eq 0 ]; then
+        echo "服务已设置为开机自启动。"
+    fi
+    sudo systemctl status chfs.service
+
+    echo "服务器已安装完成，如需再进行进一步配置请修改 /opt/chfs/config/conf.ini"
+    echo "并执行 sudo systemctl restart chfs.service 重启服务"
 else
     echo "请安装 Git 后继续。"
 fi
